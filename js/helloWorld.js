c046171f8ee69b6c66cf74ad0396068101b81d1f
Liwanx222 <79998147+Liwanx222@users.noreply.github.com>
2022-05-24 11:22:57 +0200

    changes made

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -3,4 +3,16 @@ let express = require("express");
 
 const app = express();
 
-a
+app.get("/", function (req, res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	fs.readFile("../index.html", (err, data)=>{
+		res.end(data);
+	});
+});
+
+app.get("/hello/:name", function (req, res) {
+	res.writeHead(200, {"Content-Type": "application/json"});
+	res.end(JSON.stringify({message: "Hello" + req.params.name + "!"}));
+});
+
+app.listen(8080);
