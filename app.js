9266f90146e65d88243459facab3c995f749fa37
Kilian Nagel <nagelkilian05@gmail.com>
2022-06-07 14:58:37 +0200

    changes made

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+
+/* Imports
+=============== */
+
 let fs = require('fs');
 let express = require("express");
 let path = require('path');
 let body_parser = require('body-parser');
-let db = require('./js/database');
+const req = require('express/lib/request');
+let input_handling = require('.js/')
 let db_json = './json/database.json';
+
+/* App Parsers 
+=============== */
 
 const app = express();
 app.set('view engine', 'ejs'); 
@@ -11,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 app.use(body_parser.urlencoded({extended:false}));
+
+/* Main
+=============== */
 
 app.get("/", (req, res)=> {
 	res.render("index.ejs");
@@ -21,20 +32,13 @@ app.get("/hello/:name", function (req, res) {
 	res.end(JSON.stringify({message: "Hello" + req.params.name + "!"}));
 });
 
-app.get("/express01", (req, res) => {
-	res.render("pages/express01")
+app.get('/send',(req,res)=>{
+	console.log('hello');
+	res.redirect('/');
+	fs.readFile('./json/database.json','utf-8',async(err,data)=>{
+		let db = JSON.parse(data)['messages'];
+		addMessage
+	})
 });
-
-app.get("/send",(req,res)=>{
-	res.render('index.ejs');
-	fs.readFile(db_json,'utf-8',async(err,data)=>{
-		let object_db = JSON.parse(data);
-		let item = sessionStorage.getItem('messages');
-		console.log(item);
-		let session_db = JSON.parse(item);
-		db.fillDB(session_db['messages'],object_db);
-		console.log(object_db);
-	});
-})
 
 app.listen(3000);
